aab7c3ef0705b6fa21a2ecf8ca4bbd3d62635bab
Siheon Kim <siheon0411@gmail.com>
2023-09-03 18:12:16 +0900

    applied to UpdateInputText

--- Experiment/InputManager.cs
+++ Experiment/InputManager.cs
@@ -53,42 +53,42 @@ public class InputManager : MonoBehaviour
 
     public void KeyBoard1() {
         string dial = "1";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard2() {
         string dial = "2";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard3() {
         string dial = "3";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard4() {
         string dial = "4";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard5() {
         string dial = "5";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard6() {
         string dial = "6";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard7() {
         string dial = "7";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard8() {
         string dial = "8";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoard9() {
         string dial = "9";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
     public void KeyBoardDash() {
         string dial = "-";
-        inputText.text += dial;
+        UpdateInputText(dial);
     }
 }
